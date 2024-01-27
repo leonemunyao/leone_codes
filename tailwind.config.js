68a90ab3b43259b9904e46967bab93cfe9f3fd3d
leonemunyao <leonemunyao@gmail.com>
2024-01-27 22:51:22 +0300

    Configuration of Tailwind CSS and Installation. Added several paths to tailwind.config.js. Imported Tailwind CSS into my Vuejs project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{vue,js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{vue,js,tsx,mdx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
